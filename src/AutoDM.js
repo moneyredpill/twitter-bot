c013f5b932eebda546c92345f65840240838c7f8
moneyredpill <60269721+moneyredpill@users.noreply.github.com>
2020-01-24 13:02:28 -0500

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Hi ${name}, \n Based on your background we thought you might be interested in our daily posts and weekly new content on Financial Independence, Financial Freedom, Personal Finance, Entrepreneurship, and more. \n All the best, and happy ${dayName}! `; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
